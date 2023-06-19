add55ae02358b53b2fe476cc57a00008c04408bc
ayaabdelkareem <ayaabdelkareem94@gmail.com>
2023-06-19 15:55:42 -0700

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: alaways 0 (success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
